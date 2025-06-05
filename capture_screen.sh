a0aaddf3f41e3addc8ed2826196f7825477cc7cc
Ziz <>
2025-06-05 11:59:07 +0200

    works with keybind

--- capture_screen.sh
+++ capture_screen.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+source ~/.profile
+
 if [[ -z "$(ps -e | grep "anki")" ]]; then
     /var/lib/flatpak/exports/bin/net.ankiweb.Anki 1> /dev/null 2> /dev/null &
 fi
 
 SCRIPT_LOCATION="$(dirname "$(realpath "$0")")"
+echo $SCRIPT_LOCATION
 gnome-screenshot --area --file="$SCRIPT_LOCATION/img.png"
 tesseract "$SCRIPT_LOCATION/img.png" "$SCRIPT_LOCATION/out"
 sh "$SCRIPT_LOCATION/popup.sh"
--- popup.sh
+++ popup.sh
@@ -5,15 +5,16 @@ DECKS="$(curl -X POST  --silent http://127.0.0.1:8765 \
    -d '{"action": "deckNames","version": 6}' | jq -c .result | sed -e 's/\[\|\]/ /g' -e 's/,/!/g' -e 's/"!"/!/g' -e 's/"//g')"
 
 echo ${DECKS}
+SCRIPT_LOCATION="$(dirname "$(realpath "$0")")"
 
 yad \
 --title="Add to anki" \
 --form \
---image="img.png" \
+--image="$SCRIPT_LOCATION/img.png" \
 --field="Back:CHK" true \
 --field="Deck:CBE" "${DECKS}" \
 --field="Front" \
 --field="Back" \
 --button="Add Image:2" \
 --button="Save:0" \
---button="Cancel:1"
+--button="Cancel:1" 
