239eb1250dca062ce2dd53818027f4b335897a7f
Ziz <>
2025-06-05 21:57:01 +0200

    works

--- capture_screen.sh
+++ capture_screen.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-
+    
 if [[ -z "$(ps -e | grep "anki")" ]]; then
     /var/lib/flatpak/exports/bin/net.ankiweb.Anki 1> /dev/null 2> /dev/null &
 fi
 
-SCRIPT_LOCATION="$(dirname "$(realpath "$0")")"
-echo $SCRIPT_LOCATION
+SCRIPT_LOCATION="$(dirname "$(readlink "$0")")"
 gnome-screenshot --area --file="$SCRIPT_LOCATION/img.png"
 tesseract "$SCRIPT_LOCATION/img.png" "$SCRIPT_LOCATION/out"
 
@@ -16,7 +15,10 @@ DECKS="$(curl -X POST  --silent http://127.0.0.1:8765 \
 SCRIPT_LOCATION="$(dirname "$(realpath "$0")")"
 
 yad \
+--width=400 \
+--height=400 \
 --title="Add to anki" \
+--size fit \
 --form \
 --image="$SCRIPT_LOCATION/img.png" \
 --field="Back:CHK" true \
@@ -24,4 +26,10 @@ yad \
 --field="Front" \
 --field="Back" \
 --button="Save:0" \
---button="Cancel:1" > "$SCRIPT_LOCATION/card.txt" && "$SCRIPT_LOCATION/pdf_to_anki.py" < "$SCRIPT_LOCATION/card.txt" 1> "$SCRIPT_LOCATION/debug.txt" 2> "$SCRIPT_LOCATION/debug.txt"
+--button="Cancel:1" > "$SCRIPT_LOCATION/card.txt"
+
+retVal=$?
+
+if [ $retVal -eq "0" ]; then
+    python3.13 "$SCRIPT_LOCATION"/pdf_to_anki.py "$(< $SCRIPT_LOCATION/card.txt)" >> "$SCRIPT_LOCATION/debug.txt" 2>>"$SCRIPT_LOCATION/debug.txt" 
+fi
--- pdf_to_anki.py
+++ pdf_to_anki.py
@@ -6,27 +6,23 @@ import sys
 import time
 import shutil
 import hashlib
+import os
 
+directory_path = os.path.dirname(os.path.abspath(__file__))
 
 for x in sys.argv:
     print(x)
 
-    
+
 if(len(sys.argv) < 2):
     print("No input given!")
     sys.exit(1)
 
-input = sys.argv[1].split("|")
-on_back = bool(input[0])
-deck = input[1]
-front = input[2]
-back = input[3]
-
 ANKI_USER_PATH = "/home/zcrank/.var/app/net.ankiweb.Anki/data/Anki2/User 1/"
 
-my_model = genanki.Model(
+img_back_model = genanki.Model(
   1607392319,
-  'Simple Model',
+  'Back Image OCR Model',
   fields=[
     {'name': 'Front'},
     {'name': 'Back'},
@@ -41,11 +37,35 @@ my_model = genanki.Model(
     },
   ])
 
+img_front_model = genanki.Model(
+  1607392319,
+  'Front Image OCR Model',
+  fields=[
+    {'name': 'Front'},
+    {'name': 'Back'},
+    {'name': 'Image'},
+    {'name': 'OCR'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Front}}<br>{{Image}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Back}}',
+    },
+  ])
+
 img_hash = int(time.time())
 anki_img_path = f"{ANKI_USER_PATH}/collection.media/{img_hash}.png"
-shutil.copyfile("img.png", anki_img_path)
+shutil.copyfile(os.path.join(directory_path, "img.png"), anki_img_path)
 
-my_fields = [sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else ""]
+input = sys.argv[1].split("|")
+on_back = True if input[0] == "TRUE" else False
+print(on_back)
+deck = input[1]
+front = input[2]
+back = input[3]
+
+my_fields = [front, back]
 my_fields.append(f'<img src="{img_hash}.png">')
 
 with open("out.txt", "r") as f:
@@ -53,7 +73,7 @@ with open("out.txt", "r") as f:
 
 
 my_note = genanki.Note(
-  model=my_model,
+  model=img_back_model if on_back else img_front_model,
   fields=my_fields)
 
 deck_name = deck
